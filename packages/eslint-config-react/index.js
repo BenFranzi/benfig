fc5fe8afa111d6522190e39aa3aa8a3e040e0bce
Ben Franzi <me@benfranzi.com>
2023-12-17 21:03:25 +1100

    feature: enable a11y

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,34 +1,35 @@
-module.exports = {
-  "plugins": ["jsx-a11y"],
-  "env": {
-    "browser": true,
+export default {
+  'plugins': ['jsx-a11y', 'react-refresh'],
+  'env': {
+    'browser': true,
   },
-  "extends": [
+  'extends': [
     '@bengineer.dev/core',
+    'plugin:jsx-a11y/strict'
   ],
   rules: {
-    "react/display-name": "off",
-    "react/jsx-quotes": ["error", "single", "avoid-escape"],
-    "react/jsx-no-undef": "error",
-    "react/jsx-sort-props": ["error", {
-      "callbacksLast": true,
-        "shorthandFirst": true,
-        "multiline":"first",
-        "ignoreCase": true,
-        "reservedFirst": true,
-        "locale": "auto"
+    'react/jsx-quotes': ['error', 'single', 'avoid-escape'],
+    'react/jsx-no-undef': 'error',
+    'react/jsx-sort-props': ['error', {
+      'callbacksLast': true,
+        'shorthandFirst': true,
+        'multiline':'first',
+        'ignoreCase': true,
+        'reservedFirst': true,
+        'locale': 'auto'
     }],
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "react/no-did-mount-set-state": "error",
-    "react/no-did-update-set-state": "error",
-    "react/no-multi-comp": "off",
-    "react/no-unknown-property": "error",
-    "react/prop-types": "error",
-    "react/react-in-jsx-scope": "error",
-    "react/self-closing-comp": "error",
-    "react/wrap-multilines": "error",
-    "react/jsx-curly-spacing": ["error", { "when": "always" }],
-    "react/jsx-max-props-per-line": ["error", { "maximum": 3 }]
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-did-mount-set-state': 'error',
+    'react/no-did-update-set-state': 'error',
+    'react/no-multi-comp': 'off',
+    'react/no-unknown-property': 'error',
+    'react/prop-types': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/self-closing-comp': 'error',
+    'react/wrap-multilines': 'error',
+    'react/jsx-curly-spacing': ['error', { 'when': 'always' }],
+    'react/jsx-max-props-per-line': ['error', { 'maximum': 3 }],
+    'react-refresh/only-export-components': ['error', { 'allowConstantExport': true }]
   }
 }
---
+++ tasks.md
@@ -0,0 +1,8 @@
+# Tasks
+- [ ] eslint-config-core test
+- [ ] eslint-config-react test
+- [ ] tsconfig-core test
+- [ ] tsconfig-react test
+- [ ] tsconfig-node test
+- [ ] project wide eslint
+- [ ] Publish Github action with release tags per project
